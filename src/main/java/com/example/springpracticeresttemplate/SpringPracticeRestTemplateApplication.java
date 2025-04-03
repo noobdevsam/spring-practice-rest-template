3064b982e887eaceeb829f60558bb00fd9db303a
noobdevsam <mdshoykotislam05@gmail.com>
2025-04-04 00:10:12 +0600

    initial commit

---
+++ src/main/java/com/example/springpracticeresttemplate/SpringPracticeRestTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.springpracticeresttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracticeRestTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPracticeRestTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springpracticeresttemplate/SpringPracticeRestTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springpracticeresttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeRestTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
