1ea2e15fd20f7d0a5bcd19e3f8d9f9bfd72481e3
noobdevsam <mdshoykotislam05@gmail.com>
2025-04-04 13:34:05 +0600

    implement BeerClientImpl with RestTemplate for beer listing and add integration test

--- src/main/java/com/example/springpracticeresttemplate/client/BeerClientImpl.java
+++ src/main/java/com/example/springpracticeresttemplate/client/BeerClientImpl.java
@@ -1,12 +1,34 @@
 package com.example.springpracticeresttemplate.client;
 
 import com.example.springpracticeresttemplate.model.BeerDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+@Service
+@RequiredArgsConstructor
 public class BeerClientImpl implements BeerClient {
+
+    private final RestTemplateBuilder restTemplateBuilder;
 
     @Override
     public Page<BeerDTO> listBeers() {
+
+        // Create a RestTemplate instance
+        RestTemplate restTemplate = restTemplateBuilder.build();
+
+        // Define the URL for the API endpoint
+        String api_url = "http://localhost:9090/api/v1/beer";
+
+        // Make the API call and get the response
+        // Get the response as a String and bound it to a ResponseEntity
+        ResponseEntity<String> response = restTemplate.getForEntity(api_url, String.class);
+
+        System.out.println(response.getBody());
+
         return null;
     }
 
