766ab932f2ae7a647b1859c3afe4ac98eac97758
noobdevsam <mdshoykotislam05@gmail.com>
2025-04-07 16:10:15 +0600

    rename RestTemplateBuilderConfig to RestTemplateConfig and add RestTemplate bean

--- src/main/java/com/example/springpracticeresttemplate/config/RestTemplateConfig.java
+++ src/main/java/com/example/springpracticeresttemplate/config/RestTemplateConfig.java
@@ -5,10 +5,11 @@ import org.springframework.boot.autoconfigure.web.client.RestTemplateBuilderConf
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
 @Configuration
-public class RestTemplateBuilderConfig {
+public class RestTemplateConfig {
 
     @Value("${rest.template-base-url}")
     String baseUrl;
@@ -21,5 +22,10 @@ public class RestTemplateBuilderConfig {
         var builder = configurer.configure(new RestTemplateBuilder());
         var uriBuilderFactory = new DefaultUriBuilderFactory(baseUrl);
         return builder.uriTemplateHandler(uriBuilderFactory);
+    }
+
+    @Bean
+    RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder.build();
     }
 }
